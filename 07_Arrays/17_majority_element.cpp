f7d3f54a02b6203583fccc67c828e62a65fed2af
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2025-10-09 17:48:02 +0530

    Complete Majority Element

---
+++ 07_Arrays/17_majority_element.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+// Problem: An array with a element occuring more than n/2 times. Report that answer 
+
+//* Method - I (Brute Force)
+// Make a nested loop to get the requency of each element in it and then if > n/2, then report the answer
+
+//. T.C -> O(n^2)
+//. S.C -> O(1)
+
+void bruteApproach (int n, int arr[]) {
+    int ans = -1;
+
+    for (int i = 0; i < n; i++) {
+        int cnt = 0;
+        for (int j = 0; j < n; j++){
+            if (arr[i] == arr[j]) cnt++;
+        }
+
+        if (cnt > n/2) {
+            ans = cnt;
+            break;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+//* Method - II (Better Solution)
+// Hashing - Store the freq. of all the elements and check in the map which has > n/2 occurence
+
+//. T.C -> O(nlog(n)) + O(n)
+//. S.C -> O(n)
+void betterSolution (int n, int arr[]) {
+    map<int, int>mp;
+
+    // O(n*log(N))
+    for (int i = 0; i < n; i++) {
+        mp[arr[i]]++;
+    }
+
+    // O(N)
+    for (auto elem : mp) {
+        if (elem.second > n/2) {
+            cout << elem.first << endl;
+            break;
+        }
+    }
+
+    return;
+}
+
+//* Method - III (Optimal Solution)
+//! Moore's Voting Algorithm
+
+//. T.C -> O(n) + O(n), the second one will only occur if we also verify for majority element
+//. S.C -> O(1)
+void optimalApproach (int n, int arr[]) {
+    int i = 0;
+    int elem = arr[i];
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        // If cnt = 0, reset the element
+        if (cnt == 0) elem = arr[i];
+
+        // Increase the counter if we get the chosen one
+        if (arr[i] == elem) cnt++;
+        
+        else cnt--;
+
+
+    }
+
+    //! Verify if the majority elem actually exists (not necessary, if we know that there actually does exists a majority element)
+    int occ = 0;
+    for (int i = 0; i < n; i++) {
+
+        if (arr[i] == elem) occ++;
+    }
+
+    if (occ > n/2) cout << elem << endl;
+    else cout << -1 << endl;
+}
