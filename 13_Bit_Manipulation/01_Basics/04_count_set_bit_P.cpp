0c4a0e68ad3e5ddab6e849f0ae3b240b772f716e
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2026-02-19 01:17:23 +0530

    Add insight

--- 13_Bit_Manipulation/01_Basics/04_count_set_bit_P.cpp
+++ 13_Bit_Manipulation/01_Basics/04_count_set_bit_P.cpp
@@ -42,3 +42,5 @@ int optimalApproach(int n) {
         return ans;
         
 }
+
+//! If number of set bits is only one, then it is of power of 2, this is what we did for the previous question. We do n&(n-1) once and check if it becomes 0, if it does, it mean that only one '1' bit was in the nummber, hence making the number of the power of two, else it ain't that.
