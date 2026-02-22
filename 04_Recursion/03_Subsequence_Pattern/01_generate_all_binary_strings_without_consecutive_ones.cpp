481449f41669c71d6ce4bad2bd7dbd299afd3ebe
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2026-02-23 02:03:41 +0530

    Complete generate valid parentheses

--- 04_Recursion/03_Subsequence_Pattern/01_generate_all_binary_strings_without_consecutive_ones.cpp
+++ 04_Recursion/03_Subsequence_Pattern/01_generate_all_binary_strings_without_consecutive_ones.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 // Problem Statement: Given an integer n, return all binary strings of length n that do not contain consecutive 1s. Return the result in lexicographically increasing order.
 
-//* Method - I (Recursive Approach)
+//* Method - I (Brute Recursive Approach)
+// Generate all the strings possible using similar technique as below and check at the end if valid, this take additional O(n) at the end for checking
+
+//! Total strings that can be created (valid + non-valid) is 2^n and O(n) time taken to create them (as we are iterating to each index) and O(n) for checking if valid or not at the end
+//. T.C -> O(2^n * (n + n))
+//. S.C -> O(n)
+
+//* Method - II (Optimised Recursive Approach)
 // Start with a empty string, then first generate the string with 0 at the back of the string and another function that adds 1 to the back, also make sure the previous index didn't had already '1', becuase then generating a string with 1 at current position would make it not possible.
 // At the end when the size of the string reaches n, then count it
 
-//. T.C -> O(2^n), as there are n positions and either we will have 0 or 1 at the position, giving a TC of 2^n
-//. S.C -> O(n), due to recursive stack space
+//! Total valid strings that will be generated will be less than 2^n and to generate a string it takes n T.C, as we need to iterate to all the indices, hence about TC is less than 2^n * n
+//. T.C -> O(1.618^n * n), as this is fibonacci growth so total valid strings is about 1.618^n (obviously less than 2^n)
+//. S.C -> O(n), due to recursive stack space (excluding the space for storing the ans)
 
 void generate(int n, string curr, vector<string>& ans) {
     // Base case: if length is n, add to result
---
+++ 04_Recursion/03_Subsequence_Pattern/02_generate_parentheses.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//* Method - I (Brute Recursive Approach)
+// Generate all possible strings and then check for each at the end.
+
+// Start with a empty string, then recursviely first push opening bracket and closing bracket. When the size hits 2*n, check using another function if it is valid (opening == closing and opening >= closing at any given point)
+
+// Check if a string has valid parentheses
+
+//. T.C -> O(2^(2n) * (2n + 2n)), length of string is 2n, total strings generated is 2^(2n) and to create each string it take O(2n) TC and also to check if valid or not it takes O(2n)
+//. S.C -> O(2n), due to recursive stack space (excluding space taken to store the answer)
+
+bool isValid(string s) {
+    int balance = 0;
+    for (char c : s) {
+        if (c == '(') balance++;
+        else balance--;
+        if (balance < 0) return false;
+    }
+    return balance == 0;
+}
+
+void generateAll(string curr, int n, vector<string>& res) {
+    if (curr.length() == 2 * n) {
+        if (isValid(curr)) res.push_back(curr);
+        return;
+    }
+    generateAll(curr + '(', n, res);
+    generateAll(curr + ')', n, res);
+}
+
+vector<string> generateParenthesis(int n) {
+    vector<string> res;
+    generateAll("", n, res);
+    return res;
+}
+
+//* Method - II (Optimised Recursive Approach)
+// Only generate further opening, if have less than n opening bracket already
+// Only generate further closing, if have closing less than opening
+// At the end, add the string directly into the ans array without the need to check if possible or not, as all the strings creaated is a valid
+
+//! Total number of valid parentheses is equal to the Catalan Number
+// To create each string it takes O(2n) time complexity (length of string)
+// C(n) = (2n)! / (n! * (n+1)!)
+//. T.C -> O(C(n) * (2n)) 
+//. S.C -> O(2n), due to recursive stack space (excluding answer), to store the answer in ans array, it takes an additional (C(n) * n) space
+
+void generate(int n, string s, int opening, int closing, vector<string>& ans) {
+        if (s.size() == 2*n) {
+            ans.push_back(s);
+            return;
+        } 
+
+        // Generate opening further if possible
+        if (opening < n) generate(n, s+"(", opening+1, closing, ans);
+
+        // Generate closing further only if possible
+        if (closing < opening) generate(n, s+")", opening, closing+1, ans);
+
+}
+
+vector<string> optimalRecursive(int n) {
+
+        vector<string>ans;
+
+        generate(n, "", 0, 0, ans);
+        
+        return ans;
+        
+}
