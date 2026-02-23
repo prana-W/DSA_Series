47db0d2609722d3963431235126d2b720479bdbc
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2026-02-23 14:31:00 +0530

    Fix file structure

---
+++ 18_Dynamic_Programming/01_1D_DP/02_frog_jump.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem: https://www.geeksforgeeks.org/problems/geek-jump/1
+
+// Problem Statement: Given an integer array height[] where height[i] represents the height of the i-th stair, a frog starts from the first stair and wants to reach the last stair. From any stair i, the frog has two options: it can either jump to the (i+1)th stair or the (i+2)th stair. The cost of a jump is the absolute difference in height between the two stairs. Determine the minimum total cost required for the frog to reach the last stair.
+
+//* Method - I
