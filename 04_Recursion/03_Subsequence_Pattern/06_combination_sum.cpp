11f5f814968cda433f780b56030ea7b796369162
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2026-03-17 15:10:31 +0530

    Complete Combination Sum

---
+++ 04_Recursion/03_Subsequence_Pattern/06_combination_sum.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// We have two options. Pick a element and stay at the same index (as we can take it in next steps), or not pick a element and move on.
+
+//. T.C -> O(2^t * k), k is the average length of the array
+//. S.C -> O(k*x), k is the average length and x is the total combinations; total space for answer (we have not includede recurisve stack space)
+void comb(vector<int>& arr, int index, int& sum, int target, vector<int>& container, vector<vector<int>>& ans) {
+        if (index == arr.size() || sum > target) return;
+        if (sum == target) {
+            ans.push_back(container);
+            return;
+        }
+
+        // Include (with same index)
+        container.push_back(arr[index]);
+        sum += arr[index];
+
+        comb(arr, index, sum, target, container, ans);
+
+        // For Backtracking
+        sum -= arr[index];
+        container.pop_back();
+
+        // Exclude (with next index)
+        comb(arr, index+1, sum, target, container, ans);
+
+}
+
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+
+        vector<vector<int>>ans;
+        vector<int>container;
+        int sum = 0;
+
+        comb(candidates, 0, sum, target, container, ans);
+
+        return ans;  
+}
