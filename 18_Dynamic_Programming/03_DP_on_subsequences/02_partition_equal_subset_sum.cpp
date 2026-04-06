8e96d687f20f8373d6f66f7868b6ee766fbf218a
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2026-04-06 19:26:53 +0530

    Complete Partition Equal Subset Sum

---
+++ 18_Dynamic_Programming/03_DP_on_subsequences/02_partition_equal_subset_sum.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//! We need to parition the array into exactly two subsets with equal sum. Also no elements should be left begind, hence in other words we need to check if the sum of any of the subset is equal to total_sum/2, then the sum of the other subset will also be total_sum/2 and hence such a subset will exist
+
+//* Solve like previous problem, just pass target = total_sum/2
+
+//* Other Method
+bool solve(int ind, int sum1, int sum2, vector<int>& nums, vector<vector<int>>& dp) {
+
+    if(ind == nums.size() && sum1 == sum2 && sum1 != 0) return true;
+    if(ind >= nums.size()) return false;
+
+    if(dp[ind][sum1] != -1) return dp[ind][sum1];
+
+    // Pick the element in the first subset
+    bool pickInFirst = solve(ind+1, sum1+nums[ind], sum2, nums, dp);
+    // Pick the element in the second subset
+    bool pickInSecond = solve(ind+1, sum1, sum2+nums[ind], nums, dp);
+
+    return dp[ind][sum1] = (pickInFirst || pickInSecond);
+}
+bool canPartition(vector<int>& nums) {
+    
+    int sum = 0;
+    for(auto elem : nums) {
+        sum += elem;
+    }
+
+    // We have two independent states, ind and sum1 (sum2 = sum-sum1, so it is depenededntt on sum1, now just use simple DP to check it)
+    vector<vector<int>>dp(nums.size()+1, vector<int>(sum+1, -1));
+
+    return solve(0, 0, 0, nums, dp);
+    
+}
