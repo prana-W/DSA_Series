0e4410dadbc9ebfb3503f600c88c3bff76c932f1
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2026-04-17 11:46:57 +0530

    Complete why PQ is better than Q

--- 19_Graphs/04_Shortest_Path_Algorithms/03_dijkstra_algorithm.cpp
+++ 19_Graphs/04_Shortest_Path_Algorithms/03_dijkstra_algorithm.cpp
@@ -7,17 +7,23 @@ using namespace std;
 //! Concept: Dijstra's Algorithm never works for negative weighted graphs
 // Reason, if node 0 and 1 has a weight of -2, then first (0, 0) in PQ, then as 1 is neighbour, so (-2, 1) in PQ, then again since 0 is neighbour of 1, so (-4, 0) and this makes an infinite cycle of pushing the nodes in PQ again and again!!
 
+//! Concept: This will also work if we use queue instead of PQ (min-heap) or set, but to avoid unncessary work and improving time complexity we use this appraoch by using min-heap or set
+
+// This is because we are using a greedy approach of doing the job on the minimum distance first, dry run with queue and compare why it is better
+
 //* Method - I (Using Min-Heap (PQ))
 //! For undirected weighted graph
 
 // Initialise the dist array with src having 0 dist, and push the (dist, node) in pq, then for all the adjancet nodes, find the one that is smaller than previous and then update the dist array and push in the pq
 
-//. T.C -> O(E*log(V))
+//. T.C -> O(E*log(V)), E is total number of edges and V is the number of nodes
+// O(V^2 * log(heap_size)) -> O(E*log(V^2)) -> O(E*log(V)), (V^2 -> E)
 
 vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
     
     vector<vector<pair<int, int>>>adj(V);
     
+    // O(E)
     for (int i = 0; i < edges.size(); i++) {
         int u = edges[i][0];
         int v = edges[i][1];
@@ -35,17 +41,19 @@ vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
     dist[src] = 0;
     pq.push({0, src});
     
+    // Outer loop for about O(V)
     while(!pq.empty()) {
-        auto [d, node] = pq.top();
+        auto [d, node] = pq.top(); // O(log(heap_size))
         pq.pop();
         
+        // O(V) in worst
         for (auto elem : adj[node]) {
             int nei = elem.first;
             int weight = elem.second;
             
             if (d + weight < dist[nei]) {
-                dist[nei] = (dist[node] + weight);
-                pq.push({dist[nei], nei});
+                dist[nei] = (d + weight);
+                pq.push({dist[nei], nei}); // O(log(heap_size))
             }
         }
     }
@@ -88,7 +96,7 @@ vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
             int weight = elem.second;
             
             if (d + weight < dist[nei]) {
-                dist[nei] = (dist[node] + weight);
+                dist[nei] = (d + weight);
                 st.insert({dist[nei], nei});
             }
         }
